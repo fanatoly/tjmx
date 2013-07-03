cff8ab314682019a83f186d692d966273c33bf73
Anatoly Fayngelerin <anatoly@yieldmo.com>
2013-07-03 18:34:48 -0400

    this is actually producing metrics now. Testing with TCollector.

---
+++ scripts/tjmx_collector.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+/bin/env sudo -u tomcat7 JVM_OPT="-Xbootclasspath/a:/usr/lib/jvm/jdk1.7.0/lib/tools.jar" /home/fanatoly/workspace/tjmx/target/pack/bin/tjmx 
--- src/main/scala/tjmx/MBeanPrinter.scala
+++ src/main/scala/tjmx/MBeanPrinter.scala
@@ -9,12 +9,13 @@ class MBeanPrinter(queries: List[Regex]){
 
   def output(conn: VMConnection){
     val procName = getProcessName(conn)
+    val ts = System.currentTimeMillis/1000
     conn.jmx.mbeans.foreach{ mbean =>
       mbean.attributes.foreach{
-        case dbl: RichNumberAttribute => mbean.getString(dbl).map{ printVal(procName, mbean, dbl.name, _) }
+        case dbl: RichNumberAttribute => mbean.getString(dbl).map{ printVal(procName, mbean, dbl.name, _, ts) }
         case composite: RichCompositeDataAttribute => mbean.getNumberComposite(composite).map { numMap =>
           numMap.foreach{ kv =>
-            printVal(procName, mbean, composite.name + "." + kv._1, kv._2)
+            printVal(procName, mbean, composite.name + "." + kv._1, kv._2, ts)
           }
         }
         case _ =>
@@ -22,8 +23,12 @@ class MBeanPrinter(queries: List[Regex]){
     }
   }
 
-  private def printVal[T](procName: String, mbean: RichMBean, name: String, v: T){
-    println(s"${formatMBeanName(mbean)}.${name} ${v},procName=${procName}${getTagString(mbean)}")
+  private def printVal[T](procName: String, mbean: RichMBean, name: String, v: T, ts: Long){
+    println(s"${formatMBeanName(mbean)}.${name} ${ts} ${v} procName=${formatVal(procName)} ${getTagString(mbean)}")
+  }
+
+  private def formatVal(v: String) = {
+    v.replace(' ', '_').replace("\"", "")
   }
 
   private def formatMBeanName(mbean: RichMBean): String = {
@@ -32,7 +37,12 @@ class MBeanPrinter(queries: List[Regex]){
   }
 
   private def getTagString(mbean: RichMBean): String = {
-    Option(mbean.objectName.getKeyProperty("name")).map{ ",name=" + _ }.getOrElse("")
+    // Option(mbean.objectName.getKeyProperty("name")).map{ "name=" + _ }.getOrElse("")
+    mbean.objectName.getKeyPropertyList.entrySet.foldLeft(""){ (prev, currEntry) =>
+      if(currEntry.getKey() != "type")
+        s"${prev}  ${currEntry.getKey()}=${formatVal(currEntry.getValue())}"
+      else prev
+    }
   }
 
   private def getProcessName(conn: VMConnection) = {
